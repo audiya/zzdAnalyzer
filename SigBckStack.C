63ee27f189b5bb805ea5dfc8ae1fa1d318fda14d
audiya <miss_audiya@yahoo.com>
2021-01-14 10:26:59 +0800

    Update SigBckStack.C

--- SigBckStack.C
+++ SigBckStack.C
@@ -19,15 +19,37 @@ R__LOAD_LIBRARY(libDelphes)
 void SigBckStack(){
     
   gStyle->SetOptStat(0);
-
-
-  TFile *f_sig = new TFile("/Users/applestudio/Desktop/rootFILES/4LAnalyzer_h125_4mu_zd60.root");
-  TFile *f_bkg_zz4lep = new TFile("/Users/applestudio/Desktop/rootFILES/4LAnalyzer_Background_zz4lep.root");
-  TFile *f_bkg_pph4l = new TFile("/Users/applestudio/Desktop/rootFILES/4LAnalyzer_Background_pph4l.root");
-  
-  TH1F *h_sig = (TH1F*)f_sig->Get("mZb_4mu")->Clone("h_sig");
-  TH1F *h_bkg_zz4lep = (TH1F*)f_bkg_zz4lep->Get("mZb_4mu")->Clone("h_bkg_zz4lep");
-  TH1F *h_bkg_pph4l = (TH1F*)f_bkg_pph4l->Get("mZb_4mu")->Clone("h_bkg_pph4l");
+    
+// root file
+  TFile *f_sig20 = new TFile("/Users/applestudio/Desktop/rootFILES/4LAnalyzer_h125_4e_zd20.root");
+  TFile *f_sig40 = new TFile("/Users/applestudio/Desktop/rootFILES/4LAnalyzer_h125_4e_zd40.root");
+  TFile *f_sig60 = new TFile("/Users/applestudio/Desktop/rootFILES/4LAnalyzer_h125_4e_zd60.root");
+  TFile *f_bkg_zz4lep = new TFile("/Users/applestudio/Desktop/rootFILES/4LAnalyzer_Background_ZZ4LEP500k.root");
+  TFile *f_bkg_pph4l = new TFile("/Users/applestudio/Desktop/rootFILES/4LAnalyzer_Background_pph4l500k.root");
+    
+ // get branch in tree
+  TH1F *h_sig20 = (TH1F*)f_sig20->Get("mZb_4e")->Clone("h_sig20");
+  TH1F *h_sig40 = (TH1F*)f_sig40->Get("mZb_4e")->Clone("h_sig40");
+  TH1F *h_sig60 = (TH1F*)f_sig60->Get("mZb_4e")->Clone("h_sig60");
+  TH1F *h_bkg_zz4lep = (TH1F*)f_bkg_zz4lep->Get("mZb_4e")->Clone("h_bkg_zz4lep");
+  TH1F *h_bkg_pph4l = (TH1F*)f_bkg_pph4l->Get("mZb_4e")->Clone("h_bkg_pph4l");
+    
+ // getting num of event in bin of interest  
+    double nsig20_Before = h_sig20->Integral(h_sig20->FindFixBin(15.), h_sig20->FindFixBin(120.));
+    cout << "signal_20_4e nevent Before Normalization is = " << nsig20_Before << ".\n";
+    
+    double nsig40_Before = h_sig40->Integral(h_sig40->FindFixBin(15.), h_sig40->FindFixBin(120.));
+    cout << "signal_40_4e nevent Before Normalization is = " << nsig40_Before << ".\n";
+    
+    double nsig60_Before = h_sig60->Integral(h_sig60->FindFixBin(15.), h_sig60->FindFixBin(120.));
+    cout << "signal_60_4e nevent Before Normalization is = " << nsig60_Before << ".\n";
+    
+    double nbkg_zz4lepBefore = h_bkg_zz4lep->Integral(h_bkg_zz4lep->FindFixBin(15.), h_bkg_zz4lep->FindFixBin(120.));
+    cout << "zz4lep nevent Before Normalization is = " << nbkg_zz4lepBefore << ".\n";
+    
+    double nbkg_pph4lBefore = h_bkg_pph4l->Integral(h_bkg_pph4l->FindFixBin(15.), h_bkg_pph4l->FindFixBin(120.));
+    cout << "pph4lep nevent Before Normalization is = " << nbkg_pph4lBefore << ".\n";
+   
 
   //250fb-1 = 250000pb
   float lumi = 250000;
